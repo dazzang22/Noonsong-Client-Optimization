a5285d5fa1591f386b9e59d7b80c661e7f4e79a2
dong0one <dong0one9659@sookmyung.ac.kr>
2024-09-21 19:22:36 +0900

    UI 수정 & ScreenshotHandler.cs 수정
    저장된 사진에 frameUI 안보이도록 수정

--- Assets/Scripts/ScreenshotHandler.cs
+++ Assets/Scripts/ScreenshotHandler.cs
@@ -1,21 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScreenshotHandler : MonoBehaviour
 {
+    // 캔버스를 참조로 받습니다.
+    public Canvas canvasToHide;
 
     public void CaptureScreen()
     {
+        StartCoroutine(CaptureScreenCoroutine());
+    }
+
+    private IEnumerator CaptureScreenCoroutine()
+    {
         string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
         string fileName = "Screenshot-" + timestamp;
+
+        // 캔버스 숨기기
+        canvasToHide.enabled = false;
+
+        // 한 프레임 대기 (캔버스 숨김 적용)
+        yield return new WaitForEndOfFrame();
 
         #if UNITY_IOS || UNITY_ANDROID
         CaptureScreenForMobile(fileName);
         #else
         CaptureScreenForPC(fileName);
         #endif
+
+        // 캔버스 다시 나타내기
+        canvasToHide.enabled = true;
     }
 
     private void CaptureScreenForPC(string fileName)
@@ -28,7 +43,7 @@ public class ScreenshotHandler : MonoBehaviour
     private void CaptureScreenForMobile(string fileName)
     {
         Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
-        
+
         // do something with texture
         NativeGallery.Permission permission = NativeGallery.CheckPermission(NativeGallery.PermissionType.Write, NativeGallery.MediaType.Image);
         if (permission == NativeGallery.Permission.Denied)
@@ -37,17 +52,16 @@ public class ScreenshotHandler : MonoBehaviour
             {
                 NativeGallery.OpenSettings();
             }
-        } 
-        
+        }
+
         string albumName = "Screenshot";
         NativeGallery.SaveImageToGallery(texture, albumName, fileName, (success, path) =>
         {
             Debug.Log(success);
             Debug.Log(path);
         });
-        
+
         // cleanup
         Object.Destroy(texture);
     }
-   
 }
