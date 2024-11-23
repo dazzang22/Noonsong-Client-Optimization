c4ae3b01a92cce2cbb58f1c5feb70e1da0a93ff7
dong0one <dong0one9659@sookmyung.ac.kr>
2024-11-24 03:00:30 +0900

    tutorial skip 기능 추가
    - skip UI 연결
    - UI Anchors 조정

--- Assets/Scripts/Tutorial/AddScript/UIController.cs
+++ Assets/Scripts/Tutorial/AddScript/UIController.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField] private Canvas friendsCanvas;
     [SerializeField] private Canvas mapCanvas;
+    [SerializeField] private Canvas skipCanvas;
+
     [SerializeField] private Button friendsButton;
     [SerializeField] private Button mapButton;
-    // Start is called before the first frame update
+    [SerializeField] private Button skipButton;
+    [SerializeField] private Button yesButton;
+    [SerializeField] private Button noButton;
+
     void Start()
     {
         friendsCanvas.gameObject.SetActive(false);
         mapCanvas.gameObject.SetActive(false);
-        friendsButton.onClick.AddListener(TogleFriendsCanvas);
+        skipCanvas.gameObject.SetActive(false);
+
+        friendsButton.onClick.AddListener(ToggleFriendsCanvas);
         mapButton.onClick.AddListener(ToggleMapCanvas);
+        skipButton.onClick.AddListener(ToggleSkipCanvas);
+        yesButton.onClick.AddListener(SkipTutorial);
+        noButton.onClick.AddListener(HideSkipCanvas);
     }
 
-    private void TogleFriendsCanvas()
+    private void ToggleFriendsCanvas()
     {
         friendsCanvas.gameObject.SetActive(!friendsCanvas.gameObject.activeSelf);
     }
@@ -26,5 +37,22 @@ public class UIController : MonoBehaviour
     private void ToggleMapCanvas()
     {
         mapCanvas.gameObject.SetActive(!mapCanvas.gameObject.activeSelf);
+    }
+
+    private void ToggleSkipCanvas()
+    {
+        skipCanvas.gameObject.SetActive(!skipCanvas.gameObject.activeSelf);
+    }
+
+    private void SkipTutorial()
+    {
+        // "MainScene"으로 전환
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    private void HideSkipCanvas()
+    {
+        // skipCanvas 비활성화
+        skipCanvas.gameObject.SetActive(false);
     }
 }
