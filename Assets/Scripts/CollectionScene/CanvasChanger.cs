c9714c1ab19d88c804d42aa3d17a2a375e4e585d
dong0one <dong0one9659@sookmyung.ac.kr>
2024-11-23 20:34:18 +0900

    포토 기능 다시 추가
    - photo기능 추가
    - UI Anchors 조정
    - 튜리의 연구실 UI 연결

--- Assets/Scripts/CollectionScene/CanvasChanger.cs
+++ Assets/Scripts/CollectionScene/CanvasChanger.cs
@@ -3,31 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CanvasChanger: MonoBehaviour
+public class CanvasChanger : MonoBehaviour
 {
-  [SerializeField] private Canvas mainCanvas;
-  [SerializeField] private Canvas popupCanvas;
-  [SerializeField] private Button popupButton;
-  [SerializeField] private Button backButton;
+    [SerializeField] private Canvas mainCanvas;
+    [SerializeField] private Canvas popupCanvas;
+    [SerializeField] private Canvas turyCanvas;
+    [SerializeField] private Button popupButton;
+    [SerializeField] private Button backButton;
+    [SerializeField] private Button turyButton;
 
-  private void Start()
-  {
-    mainCanvas.gameObject.SetActive(true);
-    popupCanvas.gameObject.SetActive(false);
-
-    popupButton.onClick.AddListener(ShowPopup);
-    backButton.onClick.AddListener(HidePopup);
-  }
-
-  private void ShowPopup()
-  {
-    popupCanvas.gameObject.SetActive(true);
-    mainCanvas.gameObject.SetActive(false);
-    }
-
-  private void HidePopup()
-  {
+    private void Start()
+    {
         mainCanvas.gameObject.SetActive(true);
         popupCanvas.gameObject.SetActive(false);
-  }
+        turyCanvas.gameObject.SetActive(false);
+
+        popupButton.onClick.AddListener(ShowPopup);
+        backButton.onClick.AddListener(HidePopup);
+        turyButton.onClick.AddListener(ToggleTuryCanvas); 
+    }
+
+    private void ShowPopup()
+    {
+        popupCanvas.gameObject.SetActive(true);
+        mainCanvas.gameObject.SetActive(false);
+    }
+
+    private void HidePopup()
+    {
+        mainCanvas.gameObject.SetActive(true);
+        popupCanvas.gameObject.SetActive(false);
+    }
+
+    private void ToggleTuryCanvas()
+    {
+        // 현재 활성 상태를 반전
+        turyCanvas.gameObject.SetActive(!turyCanvas.gameObject.activeSelf);
+    }
 }
