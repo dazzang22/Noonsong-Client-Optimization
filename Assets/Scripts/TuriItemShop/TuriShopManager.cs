8442fff0807eeb2ac7013aaa8f4532763239e484
hjjang2134 <wkdgmlwls2134@gmail.com>
2025-01-26 22:21:00 +0900

    기본 차트 기능 추가, 상점 아이템 차트 불러오기 기능 추가

---
+++ Assets/Scripts/TuriItemShop/TuriShopManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+using System.Text;
+
+public class TuriShopManager : MonoBehaviour
+{
+    public static TuriShopManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void DisplayShopItems()
+    {
+        if (BackendChart.Instance.shopItems.Count == 0)
+        {
+            Debug.LogError("상점 데이터가 없습니다.");
+            return;
+        }
+        
+        foreach (var item in BackendChart.Instance.shopItems)
+        {
+            Debug.Log($"아이템 이름: {item.ItemName}, 가격: {item.ItemShopValue}, 설명: {item.ItemInfo}");
+        }
+    }
+    
+
+}
