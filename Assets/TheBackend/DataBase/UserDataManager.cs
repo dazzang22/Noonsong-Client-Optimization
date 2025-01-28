6dec230d83e155ef911a3cfb2d00b5fcb550ed68
emily9821 <emily9821@naver.com>
2025-01-28 17:32:32 +0900

    UserDB

---
+++ Assets/TheBackend/DataBase/UserDataManager.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+using BackEnd;
+using LitJson;
+
+//테이블 이름: UserProfile
+//테이블 개별 데이터(행): UserProfileData
+public class UserDataManager
+{
+    private static UserDataManager _instance = null;
+
+    public static UserDataManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new UserDataManager();
+            }
+
+            return _instance;
+        }
+    }
+    public static UserProfileData userdata;
+    private string gameDataRowInDate=string.Empty;
+
+//회원가입 이후 최초 유저 등록
+    public void InsertUserData(string id,string pw)
+	{
+
+        var bro1 = Backend.BMember.GetUserInfo();
+
+        if (!bro1.IsSuccess())
+        {
+            Debug.LogError("에러가 발생했습니다 : " + bro1.ToString());
+            return;
+        }
+
+        JsonData userInfoJson = bro1.GetReturnValuetoJSON()["row"];
+        Debug.Log(userInfoJson.ToString());
+        UserProfileData user= new UserProfileData(userInfoJson);
+        user.setIdPw(id,pw);
+        Param param=user.ToParam();
+
+        Debug.Log("게임 정보 데이터 삽입을 요청합니다.");       
+        var bro = Backend.GameData.Insert("UserProfile", param);
+
+        if (bro.IsSuccess())
+        {
+            Debug.Log("게임 정보 데이터 삽입에 성공했습니다. : " + bro);
+            gameDataRowInDate=bro.GetInDate();
+        }
+        else
+        {
+            Debug.LogError("게임 정보 데이터 삽입에 실패했습니다. : " + bro);
+        }
+
+
+	}
+
+    //전체 회원 조회 (private table은 자기 자신 데이터만 조회할 수 있어서.. 일단 중지..)
+    public void getAllUsersList()
+    {
+        // List<string> userIdList = new List<string>();
+        // var bro = Backend.GameData.Get("UserProfile",new Where());
+        
+        // if(!bro.IsSuccess())
+        // {
+        //     Debug.Log("데이터 읽기 중 문제 발생: "+ bro);
+        // }
+
+        // LitJson.JsonData users=bro.FlattenRows();
+
+        // if(users==null || users.Count <= 0)
+        // {
+        //     Debug.Log("불러오기는 가능하지만, 데이터가 존재하지 않음.");
+        // }
+        
+        // for(int i=0;i<users.Count;i++)
+        // {
+        //     userIdList.Add(users[i]["userId"].ToString());
+        //     Debug.Log(i+userIdList[i]);
+        // }
+        // Debug.Log(string.Join(", ",userIdList));
+
+    }
+
+    //회원가입 한 날짜 (indate) 으로 특정 회원 조회 -> 회원 엔티티 반환.
+    public UserProfileData findUser(string indate)
+    {
+        Where where = new Where();
+        where.Equal("owner_inDate", indate);
+        var bro = Backend.GameData.Get("UserProfile", where, 10);
+
+        if(!bro.IsSuccess())
+        {
+            Debug.LogError(bro.ToString());
+            return null;
+        }
+
+        LitJson.JsonData gameDataListJson = bro.FlattenRows()[0];
+        Debug.Log(gameDataListJson.ToJson());
+
+        if(gameDataListJson.Count <= 0)
+        {
+            Debug.Log("데이터가 존재하지 않습니다");
+            return null;
+        }
+
+        gameDataRowInDate = gameDataListJson["inDate"].ToString();
+        UserProfileData gameDataItem = new UserProfileData();
+        gameDataItem.setUser(gameDataListJson);
+
+        Debug.Log(gameDataItem.ToString());
+        return gameDataItem;
+    }
+
+    //비밀번호 수정 -> 변경값을 전달
+    public Param ChangePassword(string newPw)
+    {
+        //비밀번호 수정 코드 작성
+        //UserProfileData에서 Param 생성 해서 return하기
+        return null;
+    }
+
+    //닉네임 변경   -> 변경 값을 전달
+    public Param ChangeNickname(string newnick)
+    {
+        UserProfileData userProfileData=findUser(Backend.UserInDate);
+        userProfileData.setNickname(newnick);
+        Param param=userProfileData.ToParam();
+        Debug.Log(param.GetJson().ToString());
+        return param;
+    }
+
+    //회원정보 수정 반영 (업데이트) -> 수정 함수로 변경 결과 (Param)를 받아서 테이블 업데이트
+    public void UpdateUserData(Param param)
+    {   
+        BackendReturnObject bro=null;
+        if(string.IsNullOrEmpty(gameDataRowInDate))
+        {
+            Debug.Log("내 제일 최신 게임 정보 데이터 수정을 요청합니다.");
+            bro = Backend.GameData.Update("UserProfile",new Where(),param);
+        }
+        else
+        {
+            Debug.Log($"{gameDataRowInDate}의 게임 정보 데이터 수정을 요청합니다.");
+            bro = Backend.GameData.UpdateV2("UserProfile", gameDataRowInDate ,Backend.UserInDate, param);
+
+        }
+        if(bro.IsSuccess())
+        {
+            Debug.Log("테이블 업데이트에 성공했습니다.");
+        }
+        else
+        {
+            Debug.LogError(bro.ToString());
+        }
+    }
+}
--- Assets/TheBackend/Script/BackendManager.cs
+++ Assets/TheBackend/Script/BackendManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks; // [변경] async 기능을 이용하기 위해서는 해당 namepsace가 필요합니다.  
 
 // 뒤끝 SDK namespace 추가
@@ -6,6 +8,7 @@ using BackEnd;
 
 public class BackendManager : MonoBehaviour
 {
+    private UserDataManager userDataManager;
     void Start()
     {
         var bro = Backend.Initialize(); // 뒤끝 초기화
@@ -26,8 +29,9 @@ public class BackendManager : MonoBehaviour
     // =======================================================
     // [추가] 동기 함수를 비동기에서 호출하게 해주는 함수(유니티 UI 접근 불가)
     // =======================================================
-    async void Test()
-    {
+// <<<<<<< Updated upstream
+//     async void Test()
+//     {
         /*await Task.Run(() => {
             
             BackendLogin.Instance.CustomSignUp("user1", "1234"); // [추가] 뒤끝 회원가입 함수
@@ -37,5 +41,30 @@ public class BackendManager : MonoBehaviour
         //BackendLogin.Instance.CustomLogin("user1", "1234"); // [추가] 뒤끝 로그인
         //BackendLogin.Instance.UpdateNickname("Noonsong"); // [추가] 닉네임 변겅
         //Debug.Log("테스트를 종료합니다.");
+
+    // async void Test()
+    // {
+    //     await Task.Run(() => {
+    //         BackendLogin.Instance.CustomSignUp("user2","1234");
+    //         BackendLogin.Instance.CustomLogin("user2","1234");
+    //         Debug.Log("차트를 가져옵니다.");
+
+            
+    //         BackendChart.Instance.ChartGet("157725");
+    //         Debug.Log("테스트를 종료합니다.");
+    //     });
+    // }
+
+
+    void Test(){
+        //BackendLogin.Instance.CustomSignUp("User3","1234");
+    
+        BackendLogin.Instance.CustomLogin("User2","1234");
+        //Debug.Log(Backend.UserInDate);
+        //BackendLogin.Instance.UpdateNickname("User3Change");
+
+        UserDataManager.Instance.getAllUsersList();
+  
+        Debug.Log("테스트를 종료합니다.");
     }
 }
