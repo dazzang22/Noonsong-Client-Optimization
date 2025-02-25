18738c9c9b6aa9c510cb91c3e9df9c5003d130c3
dong0one <dong0one9659@sookmyung.ac.kr>
2025-02-25 18:21:39 +0900

    전공눈송이와 기본눈송이 스폰비율 조정
    전공눈송이 80%
    기본눈송이 20%로 조정

--- Assets/Scripts/ObjectSpawn/PlayerObjectSpawn.cs
+++ Assets/Scripts/ObjectSpawn/PlayerObjectSpawn.cs
@@ -140,7 +140,7 @@ public class PlayerObjectSpawn : MonoBehaviour
     SpawnedObject GetRandomPrefab()
     {
         float probability = Random.Range(0f, 1f); // Generate a random float between 0 and 1
-        if (probability < 0.7f) // 70% probability for majorNoonsong
+        if (probability < 0.8f) // 80% probability for majorNoonsong
         {
             List<NoonsongEntry> filteredEntries = GetFilteredNoonsongEntries();
 
@@ -160,7 +160,7 @@ public class PlayerObjectSpawn : MonoBehaviour
             //int randomIndex = Random.Range(0, entries.Length);
             //return new SpawnedObject(entries[randomIndex].prefab, entries[randomIndex]);
         }
-        else // 30% probability for generalNoonsong
+        else // 20% probability for generalNoonsong
         {
             int randomIndex = Random.Range(0, generalNoonsong.Length);
             return new SpawnedObject(generalNoonsong[randomIndex], null);
