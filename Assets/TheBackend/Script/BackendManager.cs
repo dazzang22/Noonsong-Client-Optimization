3f6de994bfd385527e7b7bed687554b3e1f92a8c
emily9821 <emily9821@naver.com>
2025-04-30 01:47:58 +0900

    버전 관리 팝업, 공지사항 팝업 기능 추가

--- Assets/TheBackend/Script/BackendManager.cs
+++ Assets/TheBackend/Script/BackendManager.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks; // [변경] async 기능을 이용하기 위해서는 해당 namepsace가 필요합니다.  
+using TMPro;
+using UnityEngine.UI;
 
 // 뒤끝 SDK namespace 추가
 using BackEnd;
 
 public class BackendManager : MonoBehaviour
 {
-    private UserDataManager userDataManager;
+
     void Start()
     {
         var bro = Backend.Initialize(); // 뒤끝 초기화
@@ -19,6 +21,15 @@ public class BackendManager : MonoBehaviour
             Debug.Log("초기화 성공 : " + bro); // 성공일 경우 statusCode 204 Success
             string googlehash = Backend.Utils.GetGoogleHash();
             Debug.Log("구글 해시 키 : " + googlehash);
+        
+            //점검중 관리
+            Backend.ErrorHandler.OnMaintenanceError = () => {
+                Debug.Log("점검 에러 발생!!!");
+                VersionNoticeManger.Instance.SetMaintenanceNotice( "점검 중입니다.");
+                StartCoroutine(OpenNoticeUI());
+            };
+            //버전 & 공지 관리
+            CheckVersionAndNotice();
         }
         else
         {
@@ -34,6 +45,46 @@ public class BackendManager : MonoBehaviour
             //TuriShopManager.Instance.DisplayShopItems(); //테스트
         //});
     }
+
+    public void  CheckVersionAndNotice()
+    {
+        VersionNoticeManger.Instance.VersionCheck();
+        VersionNoticeManger.Instance.NoticeCheck();
+        Debug.Log($"{VersionNoticeManger.Instance.newText}");
+        Debug.Log($"{VersionNoticeManger.Instance.ispopup}");
+        if(VersionNoticeManger.Instance.ispopup)
+        {
+            StartCoroutine(OpenNoticeUI());
+        }
+        
+    }
+   
+    private IEnumerator OpenNoticeUI()
+    {
+        GameObject prefab = Resources.Load<GameObject>("Canvas)SavePoint PopupTutorial"); // 파일명만 정확히
+        if (prefab == null)
+        {
+            Debug.LogError("공지 팝업 프리팹을 찾을 수 없습니다.");
+            yield break;
+        }
+
+        GameObject popup = Instantiate(prefab);
+        popup.transform.SetAsLastSibling();
+
+        TMP_Text[] texts = popup.GetComponentsInChildren<TMP_Text>();
+        if (texts.Length >= 2)
+        {
+            texts[0].text = "공지";
+            texts[1].text = VersionNoticeManger.Instance.newText;
+        }
+
+        Button close = popup.GetComponentInChildren<Button>();
+        if (close != null)
+            close.onClick.AddListener(() => Destroy(popup));
+
+        yield return new WaitWhile(() => popup != null); // 닫을 때까지 대기
+    }
+
 
     // =======================================================
     // [추가] 동기 함수를 비동기에서 호출하게 해주는 함수(유니티 UI 접근 불가)
---
+++ Assets/TheBackend/Script/VersionNoticeManger.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using BackEnd;
+using TMPro;
+using System.Collections.Generic;
+using System.Collections;
+using LitJson;
+
+
+
+public class VersionNoticeManger  
+{   
+    //public GameObject noticePopupUI; //공지 팝업
+    private string title="공지";
+    public string newText; // 안내 text
+
+    public bool ispopup=false;
+    private static VersionNoticeManger _instance = null;
+
+    public static VersionNoticeManger Instance { get; } = new VersionNoticeManger();
+
+    public void SetMaintenanceNotice(string message)
+    {
+        title = "공지";
+        newText = message;
+        ispopup = true;
+    }
+
+    //버전 관리
+    public void VersionCheck()
+    {
+        var bro = Backend.Utils.GetLatestVersion();
+
+        if(!bro.IsSuccess())
+        {
+            Debug.LogError("버전 정보를 조회하는데 실패하였습니다.\n"+bro);
+            ispopup=false;
+            return;
+        }
+        
+        string version = bro.GetReturnValuetoJSON()["version"].ToString();
+
+        // 최신버전이 현재 기기에 저장된 버전이라면
+        if(version  == Application.version) {
+            ispopup=false;
+            return;
+        }
+
+        //현재 앱의 버전과 버전관리에서 설정한 버전이 맞지 않을 경우
+        string forceUpdate = bro.GetReturnValuetoJSON()["type"].ToString();
+        if(forceUpdate == "1") {
+            SetMaintenanceNotice("현재 사용 중인 앱 버전은 더 이상 지원되지 않습니다.\n스토어에서 업데이트 후 이용해 주세요.");
+            //newText = "현재 사용 중인 앱 버전은 더 이상 지원되지 않습니다.\n스토어에서 업데이트 후 이용해 주세요.";
+            Debug.Log("업데이트를 하시겠습니까? y/n");
+            //ispopup=true;
+        }
+        else if(forceUpdate == "2") {
+            Debug.Log("최신 버전이 존재합니다. 업데이트를 진행해 주세요.");
+            SetMaintenanceNotice("최신버전이 존재합니다. \n업데이트를 진행해주세요.");
+            //ispopup=true;
+        }
+    }
+    //공지 관리
+    public void  NoticeCheck()
+    {
+        string tempNotice = Backend.Notice.GetTempNotice();
+
+        if(string.IsNullOrEmpty(tempNotice)) {
+            ispopup=false;
+            return;
+        }
+
+        LitJson.JsonData data = JsonMapper.ToObject(tempNotice);
+        if(bool.Parse(data["isUse"].ToString())) {
+            Debug.Log(data["contents"].ToString());
+            SetMaintenanceNotice(data["contents"].ToString());
+            Debug.Log(newText);
+            //ispopup=true;
+        }
+    }
+    
+
+}
