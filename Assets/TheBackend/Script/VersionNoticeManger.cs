e30c2a7995825e73785aba230ce6ebf7fcbda137
emily9821 <emily9821@naver.com>
2025-04-30 16:19:05 +0900

    version수정

--- Assets/TheBackend/Script/VersionNoticeManger.cs
+++ Assets/TheBackend/Script/VersionNoticeManger.cs
@@ -40,6 +40,7 @@ public class VersionNoticeManger
         }
         
         string version = bro.GetReturnValuetoJSON()["version"].ToString();
+        Debug.Log($"{version},{Application.version}");
 
         // 최신버전이 현재 기기에 저장된 버전이라면
         if(version  <= Application.version) {
