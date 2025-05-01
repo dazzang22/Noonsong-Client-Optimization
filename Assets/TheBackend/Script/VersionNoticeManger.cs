5e0d96221593498490aa840bbfbf597d019ddf98
emily9821 <emily9821@naver.com>
2025-05-01 15:34:31 +0900

    버전 에러 해결

--- Assets/TheBackend/Script/VersionNoticeManger.cs
+++ Assets/TheBackend/Script/VersionNoticeManger.cs
@@ -6,6 +6,7 @@ using TMPro;
 using System.Collections.Generic;
 using System.Collections;
 using LitJson;
+using System;
 
 
 
@@ -43,23 +44,27 @@ public class VersionNoticeManger
         Debug.Log($"{version},{Application.version}");
 
         // 최신버전이 현재 기기에 저장된 버전이라면
-        if(version  <= Application.version) {
+        if(version  == Application.version) {
             ispopup=false;
             return;
         }
-
-        //현재 앱의 버전과 버전관리에서 설정한 버전이 맞지 않을 경우
-        string forceUpdate = bro.GetReturnValuetoJSON()["type"].ToString();
-        if(forceUpdate == "1") {
-            SetMaintenanceNotice("현재 사용 중인 앱 버전은 더 이상 지원되지 않습니다.\n스토어에서 업데이트 후 이용해 주세요.");
-            //newText = "현재 사용 중인 앱 버전은 더 이상 지원되지 않습니다.\n스토어에서 업데이트 후 이용해 주세요.";
-            Debug.Log("업데이트를 하시겠습니까? y/n");
-            //ispopup=true;
-        }
-        else if(forceUpdate == "2") {
-            Debug.Log("최신 버전이 존재합니다. 업데이트를 진행해 주세요.");
-            SetMaintenanceNotice("최신버전이 존재합니다. \n업데이트를 진행해주세요.");
-            //ispopup=true;
+        Version  client = new Version(Application.version);
+        Version server = new Version(version);
+        if(server>client)
+        {
+            //현재 앱의 버전과 버전관리에서 설정한 버전이 맞지 않을 경우
+            string forceUpdate = bro.GetReturnValuetoJSON()["type"].ToString();
+            if(forceUpdate == "1") {
+                SetMaintenanceNotice("현재 사용 중인 앱 버전은 더 이상 지원되지 않습니다.\n스토어에서 업데이트 후 이용해 주세요.");
+                //newText = "현재 사용 중인 앱 버전은 더 이상 지원되지 않습니다.\n스토어에서 업데이트 후 이용해 주세요.";
+                Debug.Log("업데이트를 하시겠습니까? y/n");
+                //ispopup=true;
+            }
+            else if(forceUpdate == "2") {
+                Debug.Log("최신 버전이 존재합니다. 업데이트를 진행해 주세요.");
+                SetMaintenanceNotice("최신버전이 존재합니다. \n업데이트를 진행해주세요.");
+                //ispopup=true;
+            }
         }
     }
     //공지 관리
